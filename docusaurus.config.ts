b4711663de5f39bef10c55022781b6ef97ed4f35
zjceo <zrenrique033@gmail.com>
2025-10-15 18:02:48 -0500

    Update base URL in configuration for manual deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: 'https://zjceo.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: 'manual',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
